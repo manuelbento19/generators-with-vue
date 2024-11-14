8335d4ebeed357c45a886c0ea71e7a909e90e9f2
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-14 17:50:21 +0100

    feat: generator

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <script lang="ts" setup>
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, onMounted, ref } from 'vue'
 import Card from './components/Card.vue'
 import type { Product } from './types'
 
 const products = ref<Product[]>([])
 
-async function getProducts() {
-  const request = await fetch('https://dummyjson.com/products')
+async function* getProducts() {
+  let skip = 0
+  const request = await fetch(`https://dummyjson.com/products?limit=30&skip=${skip}`)
   const result = await request.json()
-  products.value = result.products
+  if (!result?.products || result.products?.length == 0) return []
+
+  yield result.products
+  skip += 30
 }
 
+const paginateProducts = getProducts()
+
 onBeforeMount(async () => {
-  await getProducts()
+  const result = await paginateProducts.next()
+  products.value = [...products.value, ...(result.value as Product[])]
+})
+onMounted(() => {
+  const productSection = document.querySelector('#product-section')! as HTMLDivElement
+  productSection.addEventListener('scroll', async () => {
+    if (productSection.scrollHeight - productSection.scrollTop == productSection.offsetHeight) {
+      const result = await paginateProducts.next()
+      products.value = [...products.value, ...(result.value as Product[])]
+    }
+  })
 })
 </script>
 
@@ -25,8 +41,9 @@ onBeforeMount(async () => {
         </h1>
       </div>
     </header>
-    <section class="flex-1 overflow-auto h-[90vh] py-4 px-4 bg-gray-100">
+    <section class="flex-1 overflow-auto h-[90vh] py-4 px-4 bg-gray-100" id="product-section">
       <div
+        v-if="products.length > 0"
         class="max-w-screen-xl w-full mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5"
       >
         <Card v-for="item in products" :product="item" />
